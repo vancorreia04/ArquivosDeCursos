2590b212073decf8a094367f1ea986980c40f7f0
vancorreia04 <vancorreia04@gmail.com>
2023-09-07 17:51:47 -0300

    commit  full course

---
+++ ArquivosCursos/ELASNATECH/Aula11/DesafioAula11/javascriptDesafio.js
@@ -0,0 +1,7 @@
+var numero = parseInt(prompt("Digite um número inteiro"));
+if(numero % 2 === 0 && numero > 0 && numero <100 ){
+    alert("o número atende aos critérios");
+}else{
+    alert("O numero não atende aos critérios");
+}
+
---
+++ ArquivosCursos/ELASNATECH/Aula11/javascript.js
@@ -0,0 +1,50 @@
+//alert('Olá Mundo !');
+
+// var nome = prompt('Digite seu nome!');
+//alert('Seja bem vinda, '+ nome );
+
+//var n1 = parseInt( prompt('Digie o primeiro número'));
+//var n2 = parseInt(prompt('Digite o segundo número'));
+
+//alert('A soma é :' + (n1 +n2));
+
+// para número com casas decimais
+//var n1 = parseFloat(prompt('Digite o primeiro número'));
+//var n2 = parseFloat(prompt('Digite o segundo número'));
+
+//alert('A soma é '+ (n1+n2));
+
+
+//var n1 = parseFloat(prompt('Digite o primeiro número'));
+//var n2 = parseFloat(prompt('Digite o segundo número'));
+
+//alert('A soma é '+ (n1*n2));
+
+//var n1 = parseFloat(prompt('Digite o primeiro número'));
+//var n2 = parseFloat(prompt('Digite o segundo número'));
+
+//alert('A soma é '+ (n1/n2));
+
+//Operadores Logicos
+//var n1 = parseFloat(prompt('Digite  a primeira nota: '));
+//var n2 = parseFloat(prompt('Digite  a segunda nota:'));
+//var media = (n1+n2)/2;
+
+//alert('A media é '+ media);
+
+//if(media >= 7){
+//alert('Aprovado');
+//}else{
+// alert('Reprovado');
+//}
+
+//Operadores Relacioanis
+
+//var n = parseInt(prompt('Digite um número'));
+
+//if (n >= 0 && n <= 10) {
+  //  alert('O número está dentro do intervalo de 0 a 10')
+//} else {
+  //  alert('O númro está fora do intervalo de 0 a 10');
+//}
+
